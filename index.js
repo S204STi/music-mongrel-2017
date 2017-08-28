187a76cb362842b349046b81f2e4b5c2b719251e
Roy <rechase84@gmail.com>
2017-08-28 14:48:59 -0600

    first working copy with jade and stylus

--- index.js
+++ index.js
@@ -2,13 +2,15 @@
 
 var express  = require( 'express' ),
     logger   = require( 'morgan' ),
-    app      = express();
+    app      = express(),
+    template = require('jade').compileFile(__dirname + '/src/views/home.jade');
 
 app.use( logger( 'dev' ) );
 app.use( express.static( __dirname + '/static') );
 
 app.get('/', function (req, res) {
-    res.sendFile('views/home.html',{ root: __dirname });
+    var html = template({ title: 'Home' });
+    res.send(html);
 });
 
 app.listen( process.env.PORT || 3000, function () {
