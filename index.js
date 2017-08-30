ec107dffaa1b0bb4b61405f9041d4a795c0c3bf1
Roy <rechase84@gmail.com>
2017-08-30 13:53:48 -0600

    going to use skeleton css framework for mobile optimization

--- index.js
+++ index.js
@@ -3,25 +3,31 @@
 var express  = require( 'express' ),
     logger   = require( 'morgan' ),
     app      = express(),
-    template = require( 'jade' );
+    template = require( 'pug' );
 
 app.use( logger( 'dev' ) );
 app.use( express.static( __dirname + '/static') );
 
 app.get('/', function (req, res) {
-    var fn = template.compileFile( __dirname + '/src/views/home.jade' );
+    var fn = template.compileFile( __dirname + '/src/views/home.pug' );
     var html = fn({title: 'Home'});
     res.send(html);
 });
 
+app.get('/shows', function (req, res) {
+    var fn = template.compileFile( __dirname + '/src/views/shows.pug' );
+    var html = fn({title: 'Shows'});
+    res.send(html);
+});
+
 app.get('/about', function (req, res) {
-    var fn = template.compileFile( __dirname + '/src/views/about.jade' );
+    var fn = template.compileFile( __dirname + '/src/views/about.pug' );
     var html = fn({title: 'About'});
     res.send(html);
 });
 
 app.get('/contact', function (req, res) {
-    var fn = template.compileFile( __dirname + '/src/views/contact.jade' );
+    var fn = template.compileFile( __dirname + '/src/views/contact.pug' );
     var html = fn({title: 'Contact'});
     res.send(html);
 });
