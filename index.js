529f111ce0fb0721fbbdc873e65159734876fbc6
Roy <rechase84@gmail.com>
2017-08-30 10:22:55 -0600

    reworked bg image and styling a bit while not working on other important stuff

--- index.js
+++ index.js
@@ -3,18 +3,26 @@
 var express  = require( 'express' ),
     logger   = require( 'morgan' ),
     app      = express(),
-    template = require('jade').compileFile(__dirname + '/src/views/home.jade');
+    template = require( 'jade' );
 
 app.use( logger( 'dev' ) );
 app.use( express.static( __dirname + '/static') );
 
 app.get('/', function (req, res) {
-    var html = template({ title: 'Home' });
+    var fn = template.compileFile( __dirname + '/src/views/home.jade' );
+    var html = fn({title: 'Home'});
     res.send(html);
 });
 
 app.get('/about', function (req, res) {
-    var html = template({ title: 'About' });
+    var fn = template.compileFile( __dirname + '/src/views/about.jade' );
+    var html = fn({title: 'About'});
+    res.send(html);
+});
+
+app.get('/contact', function (req, res) {
+    var fn = template.compileFile( __dirname + '/src/views/contact.jade' );
+    var html = fn({title: 'Contact'});
     res.send(html);
 });
 
