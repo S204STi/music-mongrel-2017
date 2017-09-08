adf8e5d1be540d79552682d7fcdd732624b087c7
Roy <rechase84@gmail.com>
2017-09-08 12:49:55 -0600

    initial smtp error handling up

--- index.js
+++ index.js
@@ -2,14 +2,19 @@
 
 require('dotenv').config();
 
-const express  = require( 'express' ),
-      logger   = require( 'morgan' ),
-      app      = express(),
-      template = require( 'pug' ),
-      mailer   = require('nodemailer');
+const   express     = require( 'express' ),
+        app         = express(),
+        logger      = require( 'morgan' ),
+        template    = require( 'pug' ),
+        nodemailer  = require( 'nodemailer' ),
+        bodyParser  = require( 'body-parser' );
 
 app.use( logger( 'dev' ) );
 app.use( express.static( __dirname + '/static') );
+app.use( bodyParser.json() );
+app.use( bodyParser.urlencoded({
+    extended: true
+}));
 
 app.get('/', function (req, res) {
     var fn = template.compileFile( __dirname + '/src/views/home.pug' );
@@ -41,13 +46,45 @@ app.get('/thanks', function (req, res) {
     res.send(html);
 });
 
-app.post('/contact', function(req, res, next) {
-    var mail = true;
-    if( mail ) {
-        res.redirect('/thanks');
-    } else {
-        res.redirect('/error');
-    }
+app.get('/error', function (req, res) {
+    var fn = template.compileFile( __dirname + '/src/views/error.pug');
+    var html = fn({title:'Error'});
+    res.send(html);
+});
+
+app.post('/contact', function(req, res) {
+    let mailInfo = {};
+        mailInfo.name     = req.body.name;
+        mailInfo.bandName = req.body.bandName;
+        mailInfo.email    = req.body.email;
+        mailInfo.message  = req.body.message;
+    console.log('mailInfo', mailInfo);
+
+    let transporter = nodemailer.createTransport({
+        host:   process.env.MAIL_HOST,
+        port:   process.env.MAIL_PORT,
+        secure: false,
+        auth:   {
+            user: process.env.MAIL_USER,
+            pass: process.env.MAIL_PASS
+        }
+    });
+
+    let mailOptions = {
+        from: mailInfo.name + '<' + mailInfo.email + '>',
+        to:   'info@themusicmongel.com',
+        text: mailInfo.message
+    };
+
+    transporter.sendMail(mailOptions, (error,info) => {
+        if(error){
+            console.log(error);
+            res.redirect('/error');
+        } else {
+            console.log(info.messageId);
+            res.redirect('/thanks');
+        }
+    });
 });
 
 app.listen( process.env.PORT || 3000, function () {
